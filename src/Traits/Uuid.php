4646a4383ca3d02fb3377193e090036fe6c83ca0
waska14 <waska.chaduneli@gmail.com>
2018-11-01 13:02:29 +0400

    default_column_name moved to config. Changed save method, if model primary_key equals to uuid column, then it's not necessary to be in fillable array

--- src/Traits/Uuid.php
+++ src/Traits/Uuid.php
@@ -10,7 +10,7 @@ trait UuidTrait
     /**
      * Override model save method.
      *
-     * This method get uuid_columns from model or uses default "uuid" string and for each uuid column name
+     * This method get uuid_columns and for each uuid column name
      * generates uuid string and calls parent save method.
      *
      * @param array $options
@@ -19,11 +19,8 @@ trait UuidTrait
      */
     public function save(array $options = [])
     {
-        $uuid_columns = property_exists($this, "uuid_column") ? $this->uuid_column : "uuid";
-        foreach ((array) $uuid_columns as $column_name) {
-            if (in_array($column_name, $this->fillable) and !$this->$column_name) {
-                $this->setUuid($column_name);
-            }
+        foreach ($this->getUuidColumns() as $column_name) {
+            $this->setUuid($column_name);
         }
         parent::save($options);
     }
@@ -40,5 +37,22 @@ trait UuidTrait
         do {
             $this->$column_name = Uuid::get(4);
         } while (self::where($column_name, $this->$column_name)->count() > 0);
+    }
+
+    /**
+     * This private method returns array of uuid columns which must be filled with unique identificator
+     *
+     * @return array
+     */
+    private function getUuidColumns()
+    {
+        $uuid_columns = (array) (property_exists($this, "uuid_column") ? $this->uuid_column : config('waska.uuid.default_column_name'));
+        $uuid_fillable_columns       = [];
+        foreach ($uuid_columns as $column_name) {
+            if (!$this->$column_name and ($column_name == $this->primaryKey or in_array($column_name, $this->fillable))) {
+                $uuid_fillable_columns[] = $column_name;
+            }
+        }
+        return $uuid_fillable_columns;
     }
 }
